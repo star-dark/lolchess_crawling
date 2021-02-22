68393d3a567e2e648a87ea8ab8df37f8a147ccb3
stardark2 <dem1116@naver.com>
2021-02-22 14:03:02 +0000

    meta덱 추출

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-leaderboards_URL = ("https://lolchess.gg/leaderboards")
-leaderboards = requests.get(leaderboards_URL)
+menu = ""
+lolchessgg_URL = ("https://lolchess.gg/{menu}")
+lolchess = requests.get(lolchessgg_URL)
 lolchess_soup = BeautifulSoup(leaderboards.text, "html.parser")
 nick_name = ""
+def meta_deck():
+  menu = "meta"
+  if lolchess_soup.select("#wrapper > div.container-full > div.guide-meta.mt-4 > div.guide-meta__group.tier-S > div.guide-meta__group__content > div:nth-child(1) > div > div.guide-meta__deck__column.name.mr-3 > span").get_text == "HOT"
+    HOT_deck = lolchess_soup.select("#wrapper > div.container-full > div.guide-meta.mt-4 > div.guide-meta__group.tier-S > div.guide-meta__group__content > div:nth-child(1) > div > div.guide-meta__deck__column.name.mr-3").get_text().strip()
 def find_ranking():
 
 def paging():
